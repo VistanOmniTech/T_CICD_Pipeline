ada9c63b3dce1b0f58446e7527a055c75e2d42ef
VistanOmniTech <vistanomnitech52@gmail.com>
2026-01-22 15:18:47 +0530

    new

--- T_CICD_Project/settings.py
+++ T_CICD_Project/settings.py
@@ -116,8 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = "/home/ubuntu/T_CICD_Project/staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'T_CICD_app' / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
