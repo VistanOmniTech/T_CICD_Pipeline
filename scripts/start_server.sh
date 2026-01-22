056bb008c6c579a1a764388fa5a528671adb8d8c
VistanOmniTech <vistanomnitech52@gmail.com>
2026-01-22 12:29:02 +0530

    new

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,13 +4,16 @@ set -e
 APP_DIR="/home/ubuntu/T_CICD_Project"
 VENV_DIR="$APP_DIR/venv"
 
+# Run everything as ubuntu user
 sudo -u ubuntu bash <<EOF
 cd $APP_DIR
 source $VENV_DIR/bin/activate
 
+# Stop old gunicorn if running
 pkill gunicorn || true
 
-gunicorn T_CICD_Project.wsgi:application \
+# Start gunicorn using venv
+$VENV_DIR/bin/gunicorn T_CICD_Project.wsgi:application \
   --bind 0.0.0.0:8000 \
   --workers 3 \
   --daemon
